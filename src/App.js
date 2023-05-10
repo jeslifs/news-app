05275c19b1ea947da001b83c69343d6eb0ce9a97
jeslifs <97340330+jeslifs@users.noreply.github.com>
2023-05-11 05:22:13 +0530

    notes

--- src/App.js
+++ src/App.js
@@ -1,2 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
+// rcc
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  c = 'john';
+  render() {
+    return (
+      <div>
+        hello my first class based component, {this.c}
+      </div>
+    )
+  }
+}
