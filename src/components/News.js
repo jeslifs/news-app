cc25d219089efd0f8e6b7da5046b4d53bff8d342
jeslifs <97340330+jeslifs@users.noreply.github.com>
2023-05-11 22:38:42 +0530

    grid, title, description

--- src/components/News.js
+++ src/components/News.js
@@ -5,12 +5,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className='container my-3'>
+        <h2>JSF - Top Headlines</h2>
+        <div className="row">
+            <div className="col-md-4">
+                <NewsItem title='myTitle' description='Hello mf'/>
+            </div>
+            <div className="col-md-4">
+                <NewsItem title='myTitle' description='Hello mf'/>
+            </div>
+            <div className="col-md-4">
+                <NewsItem title='myTitle' description='Hello mf'/>
+            </div>
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,11 +1,21 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 // rce
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    // to customize thwe title and description using props
+    let {title, description} = this.props;
     return (
       <div>
-        This is a news item.
+        <div className="card" style={{width: "18rem"}}>
+            <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+                </div>
+        </div>
       </div>
     )
   }
