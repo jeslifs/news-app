bdb2d2b113f29fb6139d2ba0b381d2d7437b2eef
jeslifs <97340330+jeslifs@users.noreply.github.com>
2023-05-22 22:15:05 +0530

    slice to get few words, and read more on new tab

--- src/components/News.js
+++ src/components/News.js
@@ -72,7 +72,7 @@ export class News extends Component {
 
             {this.state.article.map((element)=>{
               return (<div className="col-md-4" key={element.url}>
-                      <NewsItem  title = {element.title} description = {element.description} imageUrl = {element.urlToImage} newsUrl='todo' />
+                      <NewsItem  title = {element.title.slice(0, 45)} description = {element.description.slice(0, 88)} imageUrl = {element.urlToImage} newsUrl={element.url} />
                       </div>)
             })}
         </div>
